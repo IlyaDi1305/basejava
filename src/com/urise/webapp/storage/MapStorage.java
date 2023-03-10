322608c406b5cfefa2c61c3879e73dcd7d3ebe9f
IlyaDi1305 <didorenko1305@gmail.com>
2023-03-10 11:59:54 +0400

    HW5 +2

---
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -0,0 +1,55 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapStorage extends AbstractStorage {
+    private Map<String, Resume> map = new HashMap<>();
+
+    @Override
+    protected String getIndex(String uuid) {
+        return uuid;
+    }
+
+    @Override
+    protected void doUpdate(Resume r, Object uuid) {
+        map.put((String) uuid, r);
+    }
+
+    @Override
+    protected boolean isExist(Object uuid) {
+        return map.containsKey((String) uuid);
+    }
+
+    @Override
+    protected void doSave(Resume r, Object uuid) {
+        map.put((String) uuid, r);
+    }
+
+    @Override
+    protected Resume doGet(Object uuid) {
+        return map.get((String) uuid);
+    }
+
+    @Override
+    protected void doDelete(Object uuid) {
+        map.remove((String) uuid);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return map.values().toArray(new Resume[map.size()]);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+}
