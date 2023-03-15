a150360164c36e5cef3f35f38b78ba24fe0df72b
IlyaDi1305 <didorenko1305@gmail.com>
2023-03-15 09:46:24 +0400

    HW5 +3

--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapStorage extends AbstractStorage {
-    private Map<String, Resume> map = new HashMap<>();
+    final private Map<String, Resume> map = new HashMap<>();
 
     @Override
     protected String getIndex(String uuid) {
@@ -45,7 +45,7 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public Resume[] getAll() {
-        return map.values().toArray(new Resume[map.size()]);
+        return map.values().toArray(new Resume[0]);
     }
 
     @Override
